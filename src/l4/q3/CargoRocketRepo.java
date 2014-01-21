82e0ae6375f7263b6360c8b258f4a4e5b02422cc
Henrique Vicente <henriquevicente@gmail.com>
2014-01-21 14:03:20 -0300

    Adding the CargoRocketRepo
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

---
+++ src/l4/q3/CargoRocketRepo.java
@@ -0,0 +1,72 @@
+package l4.q3;
+
+import l4.q1.CargoRocket;
+
+public class CargoRocketRepo implements l4.q2.CargoRocketRepo {
+    private CargoRocket[] cargoRockets;
+
+    @Override
+    public boolean add(CargoRocket cargoRocket) {
+        int length = cargoRockets.length;
+        CargoRocket[] newCargoRockets;
+
+        for (int counter = 0; counter < length; counter += 1) {
+            if (cargoRockets[counter] == null) {
+                cargoRockets[counter] = cargoRocket;
+                return true;
+            }
+        }
+
+        newCargoRockets = new CargoRocket[length + 1];
+
+        System.arraycopy(cargoRockets, 0, newCargoRockets, 0, 0);
+        this.cargoRockets = newCargoRockets;
+
+        cargoRockets[length] = cargoRocket;
+
+        return true;
+    }
+
+    @Override
+    public boolean remove(int id) {
+        for (int counter = 0, length = cargoRockets.length; counter < length; counter += 1) {
+            if (cargoRockets[counter] != null && cargoRockets[counter].getId() == id) {
+                cargoRockets[counter] = null;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public CargoRocket get(int id) {
+        for (CargoRocket current : cargoRockets) {
+            if (current != null && current.getId() == id) {
+                return current;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean has(int id) {
+        return (this.get(id) != null);
+    }
+
+    public boolean replace(int id, CargoRocket cargoRocket) {
+        for (int counter = 0, length = cargoRockets.length; counter < length; counter += 1) {
+            if (cargoRockets[counter] != null && cargoRockets[counter].getId() == id) {
+                cargoRockets[counter] = cargoRocket;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public CargoRocketRepo() {
+        this.cargoRockets = new CargoRocket[1];
+    }
+}
